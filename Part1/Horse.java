8ccf412472780c9178885a7e939c29307617853a
jbenisty <ec23064@qmul.ac.uk>
2024-04-16 16:57:40 +0100

    Horse.java updated with part1 specifications

--- Part1/Horse.java
+++ Part1/Horse.java
@@ -3,75 +3,64 @@ package Part1;
 /**
  * Write a description of class Horse here.
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @author Joshua Benisty
+ * @version 1
  */
-public class Horse
-{
-    //Fields of class Horse
-    
-    
-      
-    //Constructor of class Horse
+public class Horse {
+    private String name;
+    private char symbol;
+    private int distance;
+    private boolean fallen;
+    private double confidence;
+
+    // Constructor of class Horse
     /**
      * Constructor for objects of class Horse
      */
-    public Horse(char horseSymbol, String horseName, double horseConfidence)
-    {
-       
-    }
-    
-    
-    
-    //Other methods of class Horse
-    public void fall()
-    {
-        
-    }
-    
-    public double getConfidence()
-    {
-        
-    }
-    
-    public int getDistanceTravelled()
-    {
-        
-    }
-    
-    public String getName()
-    {
-        
-    }
-    
-    public char getSymbol()
-    {
-        
-    }
-    
-    public void goBackToStart()
-    {
-        
-    }
-    
-    public boolean hasFallen()
-    {
-        
+    public Horse(char horseSymbol, String horseName, double horseConfidence) {
+        this.symbol = horseSymbol;
+        this.name = horseName;
+        this.confidence = horseConfidence;
     }
 
-    public void moveForward()
-    {
-        
+    // Other methods of class Horse
+    public void fall() {
+        this.fallen = true;
     }
 
-    public void setConfidence(double newConfidence)
-    {
-        
+    public double getConfidence() {
+        return this.confidence;
     }
-    
-    public void setSymbol(char newSymbol)
-    {
-        
+
+    public int getDistanceTravelled() {
+        return this.distance;
     }
-    
+
+    public String getName() {
+        return this.name;
+    }
+
+    public char getSymbol() {
+        return this.symbol;
+    }
+
+    public void goBackToStart() {
+        this.distance = 0;
+    }
+
+    public boolean hasFallen() {
+        return this.fallen;
+    }
+
+    public void moveForward() {
+        distance++;
+    }
+
+    public void setConfidence(double newConfidence) {
+        this.confidence = newConfidence;
+    }
+
+    public void setSymbol(char newSymbol) {
+        this.symbol = newSymbol;
+    }
 }
